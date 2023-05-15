4ffae17fe49ea3961f6134f743aa2238afaca9c3
AynurZaydullin <zaydullin.aynur@yandex.ru>
2023-05-15 17:53:04 +0300

    Сделанное ДЗ по теме Spring.

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,2 +1,23 @@
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class Bus extends Transport{
+    private String model;
+    @Override
+    public void start() {
+        System.out.println(model + " заведён");
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public String toString() {
+        return "Автобус модели: " +
+                "'" + model + '\'';
+    }
 }
--- src/main/java/Transport.java
+++ src/main/java/Transport.java
@@ -1,2 +1,15 @@
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public abstract class Transport {
+    private String brand;
+
+    public abstract void start();
+
+    @Override
+    public String toString() {
+        return "Бренд транспорта: " + brand;
+    }
 }
